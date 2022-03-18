f4320e3122027b10bb8d95279ce1ec427f4d82d1
Eirik Gallefoss <eirik.gallefoss@nav.no>
2022-03-18 11:56:53 +0100

    Bruk plotly template som utgangspunkt

--- nav_template.py
+++ nav_template.py
@@ -18,15 +18,28 @@ diverging = [
     [0.9, "#06893A"],
     [1, "#0D7034"],
 ]
-layout = go.Layout(
-    {
-        "colorway": categorical,
-        "colorscale": {"diverging": diverging, "sequential": sequential},
-        "font": {"family": "Source Sans Pro", "size": 16},
-        "hovermode": "x unified",
-    }
-)
-template = {"layout": layout}
+new_layout = {
+    "colorway": categorical,
+    "colorscale": {"diverging": diverging, "sequential": sequential},
+    "font": {"family": "Source Sans Pro", "size": 16},
+    "hovermode": "x unified",
+}
 
+
+plotly_template = pio.templates["plotly"]
+
+
+data = {key: plotly_template["data"][key] for key in plotly_template["data"]}
+layout = {key: plotly_template["layout"][key] for key in plotly_template["layout"]}
+
+
+template = {
+    "data": data,
+    "layout": layout,
+}
+
+
+for key, val in new_layout.items():
+    template["layout"][key] = val
 
 pio.templates["nav"] = template
